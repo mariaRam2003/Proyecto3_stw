3b617f7e4c7c3a9a13ec15e0d4f7995ad182192b
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-05-31 17:36:27 -0600

    Header

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import './styles/Header.css';
+//imagenes
+import Logo from '../assets/logo.png';
 
 const Header = () => {
-  return <div className='bg-pink-300'>Header</div>;
+  return (
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='container-nav'>
+          {/* logo */}
+          <a href='#'>
+            <img src={Logo} alt=''/>
+          </a>
+          {/* button */}
+          <button className='btn btn-sm'>Trabaja conmigo</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -11,21 +11,26 @@ const Nav = () => {
       <nav className='nav'>
         <div className='container mx-auto'>
           <div className='nav-container'>
-            <Link to='home' activeClass='active' smooth={true} spy={true} className='nav-link'>
+            <Link to='home' activeClass='active' smooth={true} spy={true} offset={-200} className='nav-link'>
               <BsHouseDoorFill />
             </Link>
+
             <Link to='about' activeClass='active' smooth={true} spy={true} className='nav-link'>
               <BsPersonFill />
             </Link>
+
             <Link to='services' activeClass='active' smooth={true} spy={true} className='nav-link'>
               <BsClipboardData />
             </Link>
-            <Link to='work' activeClass='active'smooth={true} spy={true} className='nav-link'>
+
+            <Link to='work' activeClass='active' smooth={true} spy={true} className='nav-link'>
               <BsBriefcase />
             </Link>
+            
             <Link to='contact' activeClass='active' smooth={true} spy={true} className='nav-link'>
               <BsChatSquare />
             </Link>
+
           </div>
         </div>
       </nav>
