42e6f34cd07ca086d1bef6f3f8dd545c9bc97b30
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-05-31 13:38:04 -0600

    config, assets y components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,25 @@
-import './App.css';
-import About from './components/About';
+import React from 'react';
+// components
 import Banner from './components/Banner';
-import Contact from './components/Contact';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import About from './components/About';
 import Services from './components/Services';
 import Work from './components/Work';
+import Contact from './components/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Header/>
-      <Banner/>
-      <Nav/>
-      <About/>
-      <Services/>
-      <Work/>
-      <Contact/>
+    <div className='bg-site bg-no-g-cover overflow-hidden'>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
     </div>
   );
-}
+};
 
 export default App;
