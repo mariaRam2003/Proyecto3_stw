4162db5abb05d9db858807b1c9b5caae4e418553
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-05-31 14:39:21 -0600

    NavBar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -18,6 +18,7 @@ const App = () => {
       <Services />
       <Work />
       <Contact />
+      <div className='h-[4000px]'></div>
     </div>
   );
 };
--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import './styles/Nav.css'
+//iconos
+import { BsHouseDoorFill, BsPersonFill } from 'react-icons/bs';
+import { BsClipboardData, BsBriefcase, BsChatSquare } from 'react-icons/bs';
+//link
+import { Link } from 'react-scroll';
 
 const Nav = () => {
-    return <div>Nav</div>
-}
-
-export default Nav;
+    return (
+      <nav className='nav'>
+        <div className='container mx-auto'>
+          <div className='nav-container'>
+            <Link to='home' activeClass='active' smooth={true} spy={true} className='nav-link'>
+              <BsHouseDoorFill />
+            </Link>
+            <Link to='about' activeClass='active' smooth={true} spy={true} className='nav-link'>
+              <BsPersonFill />
+            </Link>
+            <Link to='services' activeClass='active' smooth={true} spy={true} className='nav-link'>
+              <BsClipboardData />
+            </Link>
+            <Link to='work' activeClass='active'smooth={true} spy={true} className='nav-link'>
+              <BsBriefcase />
+            </Link>
+            <Link to='contact' activeClass='active' smooth={true} spy={true} className='nav-link'>
+              <BsChatSquare />
+            </Link>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Nav;
