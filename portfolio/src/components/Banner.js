b4c378b41c91bfcf1ba1755cb075b2a3c54a3faa
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-06-01 10:01:22 -0600

    contact

--- portfolio/src/components/Banner.js
+++ portfolio/src/components/Banner.js
@@ -1,99 +1,100 @@
-import React from 'react';
-import './styles/Banner.css';
-import Image from '../assets/Avatar.svg';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { TypeAnimation } from 'react-type-animation';
-import { motion } from 'framer-motion';
-import { fadeIn } from '../variants';
-import { Link } from 'react-scroll';
+import React from "react";
+import "./styles/Banner.css";
+import Image from "../assets/Avatar.svg";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+import { Link } from "react-scroll";
 
 const Banner = () => {
   return (
-    <section 
-      className='min-h-[85vh] lg:min-h-[78vh] flex items-center' 
-      id='home'
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+      id="home"
     >
-      <div className='container mx-auto'>
-        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           {/* texto */}
-          <div className='flex-1 text-center font-secondary lg:text-left'>
-            <motion.h1 
-              variants={fadeIn('up', 0.3)} 
-              initial="hidden" 
-              whileInView={'show'} 
-              viewport={{once: false, amount: 0.7}} 
-              className='text-[55px] font-bold leading-[0.8] lg:text-[80px]'
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[80px]"
             >
               MARIA <span>RAMIREZ</span>
             </motion.h1>
-            <motion.div 
-              variants={fadeIn('up', 0.4)} 
-              initial="hidden" 
-              whileInView={'show'} 
-              viewport={{once: false, amount: 0.7}} 
-              className='mb-6 text-[36px] lg:text-[45px] font-secondary font-semibold uppercase leading-[1]'
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[45px] font-secondary font-semibold uppercase leading-[1]"
             >
-              <span className=''>
-              <TypeAnimation 
-                sequence={[
-                  '<Developer/>',
-                  2000,
-                  '<Designer/>',
-                  2000,
-                  '<Programmer/>',
-                  2000,
-                  '<Student/>',
-                  2000,
-                ]}
-                speed={50}
-                className='text-accent'
-                repeat={Infinity}
-              /></span>
+              <span className="">
+                <TypeAnimation
+                  sequence={[
+                    "<Developer/>",
+                    2000,
+                    "<Designer/>",
+                    2000,
+                    "<Programmer/>",
+                    2000,
+                    "<Student/>",
+                    2000,
+                  ]}
+                  speed={50}
+                  className="text-accent"
+                  repeat={Infinity}
+                />
+              </span>
             </motion.div>
-            <motion.p 
-              variants={fadeIn('up', 0.5)} 
-              initial="hidden" 
-              whileInView={'show'} 
-              viewport={{once: false, amount: 0.7}} 
-              className='mb-8 max-w-lg max-auto lg:mx-0'
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg max-auto lg:mx-0"
             >
               info mia bla bla bla
             </motion.p>
-            <motion.div 
-              variants={fadeIn('up', 0.6)} 
-              initial="hidden" 
-              whileInView={'show'} 
-              viewport={{once: false, amount: 0.7}} 
-              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
             >
-              <button className='btn btn-lg'>Contáctame</button>
+              <button className="btn btn-lg">Contáctame</button>
               <Link to="work" className="text-gradient btn-link">
                 Mi Portafolio
               </Link>
             </motion.div>
-            <motion.div 
-              variants={fadeIn('up', 0.7)} 
-              initial="hidden" 
-              whileInView={'show'} 
-              viewport={{once: false, amount: 0.7}} 
-              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
             >
-              <a href='https://github.com/mariaRam2003' target="_blank">
+              <a href="https://github.com/mariaRam2003" target="_blank">
                 <FaGithub />
               </a>
-              <a href='https://www.linkedin.com/in/mariarmrzg/' target="_blank">
+              <a href="https://www.linkedin.com/in/mariarmrzg/" target="_blank">
                 <FaLinkedin />
               </a>
             </motion.div>
           </div>
           {/* imagen */}
-          <motion.div 
-              variants={fadeIn('down', 0.5)} 
-              initial="hidden" 
-              whileInView={'show'} 
-              className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]"
           >
-            <img src={Image} alt='' />
+            <img src={Image} alt="" />
           </motion.div>
         </div>
       </div>
--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Contact = () => {
   return (
-    <div className='section' id='contact'>
-      Contact
-    </div>
+    <section className="py-16 lg:section" id="contact">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* texto */}
+          <motion.div
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 "
+          >
+            <h4 className="text-xl uppercase text-accent font-medium mb-2 tracking-wide">
+              Ponte en contacto conmigo
+            </h4>
+            <h2 className="text-[45px] lg:text-[90px] leading-none mb-12">
+              Trabajemos <br />
+              juntos!
+            </h2>
+          </motion.div>
+          {/* form */}
+          <motion.form
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 border rounded-xl flex flex-col p-6 mb-24 items-start"
+          >
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all mb-4"
+              type="text"
+              placeholder="Tu nombre"
+            />
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all mb-4"
+              type="text"
+              placeholder="Tu email"
+            />
+            <textarea
+              className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-4"
+              placeholder="Tu mensaje"
+            ></textarea>
+            <button className="btn btn-lg">Enviar mensaje</button>
+          </motion.form>
+        </div>
+      </div>
+    </section>
   );
 };
 
